d01024f4c8a3566a2fbf2b90e23fbaaea58f2353
YuSunjo <tnswh2023@gmail.com>
2021-07-18 18:21:54 +0900

    config 프로젝트 추가

---
+++ velog-config/src/test/java/com/velog/velogconfig/VelogConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.velog.velogconfig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VelogConfigApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
